f4835927097f4256e66c416294cda043751c4eb8
Medha_02 <65821243+ChowdhuryMedhaPaul@users.noreply.github.com>
2021-10-27 22:01:08 +0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,18 @@
+price=10.234
+price=10
+name='medha'
+bool= True
+print(price,name,bool)
+
+
+vari='hello world'
+print('Hi '+ vari)
+
+
+vari= input(' What is my name? ')
+print('Hi '+ vari)
+
+vari = input('what is your name? ')
+vari_1=input('what is your fav colour? ')
+print(vari+ 'likes'+vari_1)
+
