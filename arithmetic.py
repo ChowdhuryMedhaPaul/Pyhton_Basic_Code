83afcaeaf8032f152fef344af6b8e97b43e1eb2d
Medha_02 <65821243+ChowdhuryMedhaPaul@users.noreply.github.com>
2021-10-29 03:11:18 +0600

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,28 @@
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+
+x=10
+x=x+3
+print(x)
+x=10
+x+=3
+x-=3
+print(x)
+x=10+3*2
+y=10+3*2**2
+print(x,y)
+y=(10+3)*2**2
+print(y)
+
+
+x=2.9
+print(round(x))
+print(abs(-x))
+
+
+import math
+print(math.ceil(x))
+print(math.floor(x))
